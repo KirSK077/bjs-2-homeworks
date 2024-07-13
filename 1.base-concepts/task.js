08814de2393719aa6a828afe803baa1fe712fdd0
Kirill Kaimusov <kaimusovks@yandex.ru>
2024-07-13 17:43:19 +0500

    Сделано Домашнее задание к лекции 1 «Основные понятия»

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,27 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+
+	let d = b ** 2 - 4 * a * c;
+
+	if (d < 0) {
+		arr = [];
+	} else if (d === 0) {
+		let x = -b / (2 * a);
+		arr = [x];
+	} else {
+		let x1 = (-b + Math.sqrt(d)) / (2 * a);
+		let x2 = (-b - Math.sqrt(d)) / (2 * a);
+		arr = [x1, x2];
+	}
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let bodyPayment = amount - contribution;
+	let percentMonth = percent / (12 * 100);
+	let paymentMonth = bodyPayment * (percentMonth + (percentMonth / ((Math.pow(1 + percentMonth, countMonths)) - 1)));
+	let totalPayment = +(paymentMonth * countMonths).toFixed(2);
+	return totalPayment;
 }
